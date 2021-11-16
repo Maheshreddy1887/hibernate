da1a2eef4f55f497c48f7c585a26b35553859bd6
mahesh m <maheshreddyreddy1887@gmail.com>
2021-11-16 06:46:50 -0500

    SingleFactory

--- mobile/src/main/java/com/xworkz/mobile/MobileTester.java
+++ mobile/src/main/java/com/xworkz/mobile/MobileTester.java
@@ -1,12 +1,13 @@
 package com.xworkz.mobile;
 
 import com.xworkz.mobile.hibernate.dao.MobileDAO;
+import com.xworkz.mobile.hibernate.dao.MobileImpl;
 
 public class MobileTester {
 
 	public static void main(String[] args) {
 
-		MobileDAO mobileDAOImpl = new MobileImplDAO();
+		MobileDAO mobileDAOImpl = new MobileImpl();
 
 		// mobileDAOImpl.saveMobileEntity();
 
